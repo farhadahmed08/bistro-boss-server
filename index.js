ffc921c26eb6b6552a7a8b3370c89ded545f1827
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-18 23:50:48 +0800

    tanstack impliment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -48,6 +48,13 @@ async function run() {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result)
+    });
+
+    app.delete('/carts/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
 
