b92d7feb084295dc33917e7d0882b0190241e44e
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-23 00:56:11 +0800

    token generated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ObjectId } = require('mongodb');
@@ -22,11 +23,78 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
-    
 
+
+    //jwt related api
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'1h'
+      });
+      console.log(token)
+      // res.send({token:token})
+      res.send({token}); //short hand
+    })
+
+
+    //user related api
+    app.get('/users', async(req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+
+    app.post('/users',async (req,res)=>{
+      const user = req.body;
+      // insert email if user dosent exist 
+      // you can do this many ways (1.email unique, 2.upsert 3.simple checking)
+      const query = {email:user.email}
+      // console.log(query)
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({message:'user already exists',insertedId:null})
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)};
+      const updatedDoc ={
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+
+
+
+    })
+
+
+
+    app.delete('/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      
+      const query = {_id : new ObjectId(id)}
+     
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+
+
+    
+    //menu related apis
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
         res.send(result);
