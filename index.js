9e31834cfaadb59d763b97256e6c67a9a44dbcc6
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-24 03:34:25 +0800

    axios interceptor

--- index.js
+++ index.js
@@ -35,17 +35,58 @@ async function run() {
       const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
         expiresIn:'1h'
       });
-      console.log(token)
+      // console.log(token)
       // res.send({token:token})
       res.send({token}); //short hand
     })
 
+    //middleware
+    const verifyToken = (req,res,next)=>{
+      console.log('inside verify token',req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({message:'unauthorized access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if (err) {
+          return res.status(401).send({message:'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+     
+    }
+
 
     //user related api
-    app.get('/users', async(req,res)=>{
+    app.get('/users',verifyToken, async(req,res)=>{
+      
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+
+
+    app.get('/users/admin/:email', verifyToken, async(req,res)=>{
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({message:'forbidden access'})
+      }
+
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role ==='admin'
+      }
+      res.send({admin});
+
+
+
+    })
+
+
+
+  
 
 
     app.post('/users',async (req,res)=>{
